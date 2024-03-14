c919adba891323764b6ac10cbec876eba43686e6
Cbgogogog <1293543528@qq.com>
2024-03-14 22:57:58 +0800

    feat: 完善question

--- src/main/java/cn/wxl475/service/impl/QuestionServiceImpl.java
+++ src/main/java/cn/wxl475/service/impl/QuestionServiceImpl.java
@@ -4,6 +4,7 @@ import cn.wxl475.mapper.QuestionMapper;
 import cn.wxl475.pojo.Question;
 import cn.wxl475.pojo.User;
 import cn.wxl475.redis.CacheClient;
+import cn.wxl475.service.PaperService;
 import cn.wxl475.service.QuestionService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static cn.wxl475.redis.RedisConstants.CACHE_QUESTION_KEY;
+import static cn.wxl475.redis.RedisConstants.CACHE_QUESTION_TTL;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
@@ -23,23 +28,35 @@ public class QuestionServiceImpl implements QuestionService {
     @Override
     public Long createQuestion(Question question) {
         questionMapper.insert(question);
+        cacheClient.setWithLogicalExpire(CACHE_QUESTION_KEY + question.getQuestionId().toString(), question, CACHE_QUESTION_TTL, TimeUnit.MINUTES);
         return question.getQuestionId();
     }
 
     @Override
     public void deleteQuestion(ArrayList<Long> questionIds) {
         questionMapper.deleteBatchIds(questionIds);
+        for (Long id : questionIds) {
+            cacheClient.delete(CACHE_QUESTION_KEY + id.toString());
+        }
     }
 
     @Override
     public Long updateQuestion(Question question) {
         questionMapper.updateById(question);
+        cacheClient.setWithLogicalExpire(CACHE_QUESTION_KEY + question.getQuestionId().toString(), question, CACHE_QUESTION_TTL, TimeUnit.MINUTES);
         return question.getQuestionId();
     }
 
     @Override
     public Question getQuestionById(Long questionId) {
-        return questionMapper.selectById(questionId);
+        return cacheClient.queryWithPassThrough(
+                CACHE_QUESTION_KEY,
+                questionId,
+                Question.class,
+                id ->  questionMapper.selectById(questionId),
+                CACHE_QUESTION_TTL,
+                TimeUnit.MINUTES
+        );
     }
 
     @Override
